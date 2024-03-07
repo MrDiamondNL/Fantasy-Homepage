717cfebd1cd33c98a6cd52b7f4071b4dfb21ac80
MrDiamondNL <36870588+MrDiamondNL@users.noreply.github.com>
2024-03-07 16:52:57 -0330

    got the player search and stats pull working

--- create_player_array.js
+++ create_player_array.js
@@ -10,6 +10,8 @@ let playerLastName = "";
 let playerPosition = "";
 let playerCode = 0;
 let playerStatList = [];
+let playerPortrait = "";
+let tempPortrait = "";
 
 // const searchInput = document.querySelector(".player-search");
 // const input = searchInput.querySelector("input");
@@ -36,28 +38,31 @@ async function addPlayerToArray() {
             playerLastName = currentRoster.forwards[y].lastName.default;
             playerPosition = currentRoster.forwards[y].positionCode;
             playerCode = currentRoster.forwards[y].id;
-            appendToPlayerArray(playerFirstName, playerLastName, playerPosition, playerCode);
+            playerPortrait = currentRoster.forwards[y].headshot;
+            appendToPlayerArray(playerFirstName, playerLastName, playerPosition, playerCode, playerPortrait);
         }
         for (let y = 0; y <currentRoster.defensemen.length; y++) {
             playerFirstName = currentRoster.defensemen[y].firstName.default;
             playerLastName = currentRoster.defensemen[y].lastName.default;
             playerPosition = currentRoster.defensemen[y].positionCode;
             playerCode = currentRoster.defensemen[y].id;
-            appendToPlayerArray(playerFirstName, playerLastName, playerPosition, playerCode);
+            playerPortrait = currentRoster.defensemen[y].headshot;
+            appendToPlayerArray(playerFirstName, playerLastName, playerPosition, playerCode, playerPortrait);
         }
         for (let y = 0; y <currentRoster.goalies.length; y++) {
             playerFirstName = currentRoster.goalies[y].firstName.default;
             playerLastName = currentRoster.goalies[y].lastName.default;
             playerPosition = currentRoster.goalies[y].positionCode;
             playerCode = currentRoster.goalies[y].id;
-            appendToPlayerArray(playerFirstName, playerLastName, playerPosition, playerCode);
+            playerPortrait = currentRoster.goalies[y].headshot;
+            appendToPlayerArray(playerFirstName, playerLastName, playerPosition, playerCode, playerPortrait);
         }
     }
     console.log(playerList);
 }
 
-function appendToPlayerArray(firstName, lastName, playerPos, playerID) {
-    const newPlayer = {firstName, lastName, playerPos, playerID};
+function appendToPlayerArray(firstName, lastName, playerPos, playerID, portrait) {
+    const newPlayer = {firstName, lastName, playerPos, playerID, portrait};
     playerList.push(newPlayer);
 }
 
@@ -101,6 +106,7 @@ function pullPlayerCode() {
     for (let x = 0; x < playerList.length; x++) {
         fullName = (playerList[x].firstName).concat(" ", playerList[x].lastName);
         if (userSearch.toLowerCase() == fullName.toLowerCase()) {
+            tempPortrait = playerList[x].portrait;
             return playerList[x].playerID;
         }
     }
@@ -112,21 +118,37 @@ async function pullPlayerStats() {
     let urlPlayerStatFetch = `https://api-web.nhle.com/v1/player/${currentPlayerFetch}/landing`;
     let currentData = await getData(urlPlayerStatFetch);
     playerStatList = currentData.featuredStats.regularSeason.subSeason;
-    console.log(playerStatList);
     return playerStatList;
 }
 
-function newRosterCard() {
-    playerStatList = pullPlayerStats();
+async function newRosterCard() {
+    playerStatList = await pullPlayerStats();    
     let x = document.getElementById("player-search").value;
     document.getElementById("roster-section").innerHTML += `
     <div class = "player-card">
         <div class="player-score">
-            <h3 class="big-score">0pts</h3>
+            <img src="${tempPortrait}" class="player-portrait" />
         </div> 
         <div class="player-details">
-            <h4 class="player-name"> </h4> 
-            <p class="player-stats">G, A, +/-, PIMS, PPP, SHP, BLKs</p>
+            <h4 class="player-name">${x} </h4> 
+            <p class="player-stats">${playerStatList.goals}g, ${playerStatList.assists}a, ${playerStatList.plusMinus}, ${playerStatList.pim}pims, ${playerStatList.powerPlayPoints}ppp, ${playerStatList.shorthandedPoints}shp</p>
+            </div>
+        "<div class= "delete-card"><button class = "button-del" name ="delete-button" type ="button" onclick = "return this.parentNode.parentNode.remove();">X</button></div>
+    </div>
+    `;
+}
+
+async function newWatchlistCard(playerName) {
+    playerStatList = await pullPlayerStats();    
+    let x = document.getElementById("player-search").value;
+    document.getElementById("custom-section").innerHTML += `
+    <div class = "player-card">
+        <div class="player-score">
+            <img src="${tempPortrait}" class="player-portrait" />
+        </div> 
+        <div class="player-details">
+            <h4 class="player-name">${x} </h4> 
+            <p class="player-stats">${playerStatList.goals}g, ${playerStatList.assists}a, ${playerStatList.plusMinus}, ${playerStatList.pim}pims, ${playerStatList.powerPlayPoints}ppp, ${playerStatList.shorthandedPoints}shp</p>
             </div>
         "<div class= "delete-card"><button class = "button-del" name ="delete-button" type ="button" onclick = "return this.parentNode.parentNode.remove();">X</button></div>
     </div>
